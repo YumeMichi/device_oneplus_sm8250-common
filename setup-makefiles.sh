f939106c4f4171cfc97805354fa1ff27c85878c0
Yuan Si <do4suki@gmail.com>
2022-08-19 20:32:51 +0800

    sm8250-common: Convert to AOSPA tree
    Signed-off-by: Yuan Si <do4suki@gmail.com>
    Change-Id: Ib25678430e1dffdbf9beb79a2b16fadc41c4f1fa

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "instantnoodle instantnoodlep kebab lemonades"
+write_headers "oneplus8 oneplus8pro oneplus8t oneplus9r"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
