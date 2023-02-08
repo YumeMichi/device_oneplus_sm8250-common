1429b334c56b10cb943af7805cb3681e3d8c6446
Jake Weinstein <jake@aospa.co>
2023-02-09 01:42:04 +0900

    sm8250-common: Use common compatibility matrix
    Change-Id: I877824cc6c15cee7bdae4457984a6130430efd11

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -59,7 +59,6 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/device_framework_matrix.xml
-DEVICE_MATRIX_FILE += device/qcom/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
 # Init
