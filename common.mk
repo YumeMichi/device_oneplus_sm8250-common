ca1be32116a09f0100f22c6b6fc80edbcf9d01b5
Mashopy <eliasgheeraert@gmail.com>
2023-04-26 19:44:50 +0200

    sm8250-common: Disable memtrack support
    Change-Id: I014b5debd121d398546b678050b47621e1d242cc

--- common.mk
+++ common.mk
@@ -71,6 +71,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
+# Display
+TARGET_DISABLE_MEMTRACK := true
+
 # Doze
 PRODUCT_PACKAGES += \
     ParanoidDoze
