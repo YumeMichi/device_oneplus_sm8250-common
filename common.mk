4ab37635105422984c6d0b17c288feb9d41e9a2b
Yuan Si <do4suki@gmail.com>
2023-10-15 03:13:48 +0800

    sm8250-common: Remove vibrator component
    We are using a custom vibrator HAL so there is no reason to use common vibrator component.
    
    Change-Id: I4d63bc0135089cc37a19988d19d4dddda91a8f3d
    Signed-off-by: Yuan Si <do4suki@gmail.com>

--- common.mk
+++ common.mk
@@ -171,7 +171,6 @@ TARGET_COMMON_QTI_COMPONENTS := \
     perf \
     telephony \
     usb \
-    vibrator \
     wfd \
     wlan
 
@@ -223,6 +222,9 @@ PRODUCT_COPY_FILES += \
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.oplus
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
 PRODUCT_USE_PRODUCT_VNDK_OVERRIDE := true
