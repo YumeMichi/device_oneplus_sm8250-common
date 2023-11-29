35e37ca5818d70a798ce201b8b26b823e8063823
Omkar Chandorkar <gotenksIN@aospa.co>
2023-11-29 23:15:39 +0900

    sm8250-common: Enable Wifi overlay for mainline modules
    Change-Id: Iac8be06e58d24d868a33d212d80a583bad8705ef
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- common.mk
+++ common.mk
@@ -143,7 +143,8 @@ PRODUCT_PACKAGES += \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
     OPlusSystemUIResCommon \
-    WifiResTarget
+    WifiResTarget \
+    WifiResTargetMainline
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
